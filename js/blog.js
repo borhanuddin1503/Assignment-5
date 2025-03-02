291fa0e23b291372885a55cbf8d91f8b49f1f680
borhanuddin1503 <borhanuddin1503@gmail.com>
2025-03-02 15:00:31 +0600

    add all functionality

---
+++ js/blog.js
@@ -0,0 +1,3 @@
+document.getElementById('back_to_desk').addEventListener('click' , function(event){
+    window.location.href = 'index.html'
+})
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 const complete_btns = document.querySelectorAll('.complete_btn');
+let clickedItem = 0;
 for (const btn of complete_btns) {
     btn.addEventListener('click', function () {
         // show the title in alert
@@ -24,7 +25,7 @@ for (const btn of complete_btns) {
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
         const amOrPm = hours >= 12 ? 'PM' : 'AM';
-        hours > 12 ? hours - 12 : hours;
+        hours = hours > 12 ? hours - 12 : hours;
         const accurateTime = `${hours}:${minutes}:${seconds} ${amOrPm}`
 
         // add the task in the Activity log
@@ -38,6 +39,13 @@ for (const btn of complete_btns) {
         `;
         p.classList.add('p-[10px]' , 'mb-5' , 'rounded-[8px]' , 'bg-[#F4F7FF]' , 'leading-6' , 'font-medium');
         activityContainer.appendChild(p);
+
+        clickedItem++;
+
+        // show the congrats message
+        if(clickedItem === complete_btns.length){
+            alert('Congrates , You have completed all the current task')
+        }
     });
 }
 
@@ -51,3 +59,22 @@ document.getElementById('clear_history_btn').addEventListener('click',function()
 document.getElementById('discover_something').addEventListener('click', function(){
     window.location.href ='blog.html'
 })
+
+
+// change background on click theme icon
+document.getElementById('theme_btn').addEventListener('click' , function(){
+    const random1 = Math.ceil(Math.random()*255);
+    const random2 = Math.ceil(Math.random()*255);
+    const random3 = Math.ceil(Math.random()*255);
+
+    document.body.style.background = `rgb(${random1} , ${random2} , ${random3})`
+})
+
+// make dynamic the date
+const day = document.getElementById('day');
+const date = document.getElementById('date');
+
+day.innerText = new Date().toLocaleString("en-US", {weekday: 'short'});
+date.innerText = new Date().toLocaleString("en-US", { year: 'numeric', month: 'short', day: 'numeric'});
+;
+
